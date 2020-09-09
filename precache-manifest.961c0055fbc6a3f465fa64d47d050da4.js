c55b2d7451ae0a0bcdddd73279ab40b1064bae7c
LuisEnMarroquin <mluis651@gmail.com>
2020-09-09 07:59:59 +0000

    Deploy to gh-pages from main @ a38d677 🚀

---
+++ precache-manifest.961c0055fbc6a3f465fa64d47d050da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6277cfc15ff7d2c96930ec78bc5303",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "1fe0af01e5ef1395da6d",
+    "url": "/curriculum-vitae/static/css/main.66c81dbd.chunk.css"
+  },
+  {
+    "revision": "d819eb8ce788481b8c70",
+    "url": "/curriculum-vitae/static/js/2.529122b4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/curriculum-vitae/static/js/2.529122b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe0af01e5ef1395da6d",
+    "url": "/curriculum-vitae/static/js/main.0d474a8d.chunk.js"
+  },
+  {
+    "revision": "bc091ed61d8be975c542",
+    "url": "/curriculum-vitae/static/js/runtime-main.cce34d04.js"
+  },
+  {
+    "revision": "63cfaa6a6df12358402b190101f2071c",
+    "url": "/curriculum-vitae/static/media/profile.63cfaa6a.jpg"
+  }
+]);
