d25f491aae24fb01ad264bffa143b3c3de46fcbf
LuisEnMarroquin <mluis651@gmail.com>
2021-04-12 05:16:46 -0500

    Added mySQL and removed Debian

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -67,8 +67,8 @@ function Main() {
             </Grid>
             <Grid item xs={12} sm={12} md={4}>
               <SimpleList subtitle='Testing' contents={['Cypress', 'Puppeteer', 'SeleniumJS']} />
-              <SimpleList subtitle='Databases' contents={['MongoDB', 'SQL Server']} />
-              <SimpleList subtitle='OS' contents={['Debian', 'Ubuntu Server']} />
+              <SimpleList subtitle='Databases' contents={['MySQL', 'MongoDB', 'SQL Server']} />
+              <SimpleList subtitle='OS' contents={['Ubuntu Server']} />
             </Grid>
           </Grid>
         </Grid>
